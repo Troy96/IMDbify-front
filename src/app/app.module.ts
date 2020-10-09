3d131af2cd129e6c86bcf457adcef0a79751068e
Tuhin Roy <troy0870@gmail.com>
2020-10-09 20:19:53 +0530

    add server side rendering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
     MovieComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     HttpClientModule
   ],
